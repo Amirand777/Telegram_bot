95601bc70fa6eb20e54a06eb6845a30990e4f10e
Amirand777 <d.begmatova@icloud.com>
2025-08-10 23:02:55 +0500

    Update main.py

--- main.py
+++ main.py
@@ -112,7 +112,7 @@ async def handle_response(update: Update, context: ContextTypes.DEFAULT_TYPE):
 
 # Botni ishga tushirish
 async def main():
-    application = ApplicationBuilder().token("BOT_TOKENINGNI_QO'Y").build()
+    application = ApplicationBuilder().token("8452858160:AAHr1NxhlpAZXPFA2UpjCXFcwHiP27vZAB4").build()
 
     application.add_handler(CommandHandler("start", start))
     application.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, handle_response))
